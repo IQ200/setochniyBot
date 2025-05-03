748b08d736ed2621326bcb9ac498ac9f290de2bd
konyaevkms <konyaevkms@gmail.com>
2025-05-03 21:22:08 +0300

    1

---
+++ main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from binance.client import Client
+import config
+
+client = Client(config.API_KEY, config.API_SECRET)
+
+# Получить баланс
+balance = client.futures_account_balance()
+print(balance)
+
+
+
+
+
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
