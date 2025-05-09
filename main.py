00ecc7e6ac8a65f8691fa3fe208d5639a8dab56e
konyaevkms <konyaevkms@gmail.com>
2025-05-09 12:07:43 +0300

    1

--- main.py
+++ main.py
@@ -1,81 +1,386 @@
-import ccxt
-import time
-import logging
+# from datetime import datetime
+# from binance.error import ClientError
+# from binance.spot import Spot
+# import ccxt
+# import logging
+# import os
+# import sys
+# from binance import client
+# from binance import depthcache
+# from binance import enums
+# from binance import exceptions
+# from binance import websockets
 
+# from binance import *
+
+import time
+import datetime
+import ssl
+import websocket
+import pandas as pd
+from pandas import DataFrame
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+from binance.exceptions import BinanceAPIException
+import re
 import config
 
-from binance.um_futures import UMFutures
-from binance.websocket.um_futures.websocket_client import UMFuturesWebsocketClient
-from binance.lib.utils import config_logging
+# from binance.um_futures import UMFutures
+# from binance.websocket.um_futures.websocket_client import UMFuturesWebsocketClient
+# from binance.lib.utils import config_logging
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 
 # config_logging(logging, logging.DEBUG)
 
 
-symbol = 'EPTUSDT'
-
-setOrders = {a: 0 for a in range(21)}
-
-
-client = UMFutures(key=config.apiKey, secret=config.secretKey)
-
-# link = 'wss://fstream.binance.com/ws/'+symbol.lower()+'@trade'
+def on_msg(_, msg):
+    print(msg)
 
 
-def message_handler(_, message):
-    print(message)
+def on_err(_, err):
+    print(err)
 
 
+def main():
+    global initProc, lastPrice, sizeOrder, lenDrobChast, gridPercent, zeroFiveProcent, setGridOrders, sizeOrderUSD, quantityOrders, lenSetGridOrders, orderClientId
 
-ccxtBinance = ccxt.binance({
-            'apiKey': config.apiKey,
-            'secret': config.secretKey,
-            'enableRateLimit': True,
+    initProc = True
+    sizeOrderUSD = 5.4
+    gridPercent = 0.005  # НЕ ПРОЦЕНТЫ, А МНОЖИТЕЛЬ!!!!!!
+    quantityOrders = 2
 
-            'options': {
-                'defaultType': 'future',
-                'urls': {
-                    'api': 'fapiPublic',
-                    'test': 'https://testnet.binancefuture.com',
-                },
-            },
-        })
+    quantityOrders = int(quantityOrders / 2) * 2  # Четное!!!
+
+    setGridOrders = {a: 0 for a in range(quantityOrders + 1)}
+
+    symbol = config.symbol
+
+    lastPrice = sizeOrder = lenDrobChast = zeroFiveProcent = sizeOrder = orderClientId = 0
+
+    # create futures order
+    def create_futures_sell_limit_order(symbol, sizeOrder, price, newClientOrderId):
+        order = client.futures_create_order(
+            symbol=symbol,
+            side="SELL",
+            type="LIMIT",
+            quantity=sizeOrder,
+            price=price,
+            # positionSide="SHORT",  # BOTH for One-way Mode ; LONG or SHORT for Hedge Mode
+            newClientOrderId=newClientOrderId,
+            recvWindow=5000,
+            timeInForce="GTC",
+            timestamp=time.time_ns()
+        )
+        return (order)
+
+    def create_futures_buy_limit_order(symbol, sizeOrder, price, newClientOrderId):
+        order = client.futures_create_order(
+            symbol=symbol,
+            side="BUY",
+            type="LIMIT",
+            quantity=sizeOrder,
+            price=price,
+            # positionSide="SHORT",  # BOTH for One-way Mode ; LONG or SHORT for Hedge Mode
+            newClientOrderId=newClientOrderId,
+            recvWindow=5000,
+            timeInForce="GTC",
+            timestamp=time.time_ns()
+        )
+        return (order)
+
+    def _user_data(msg):
+
+        global orderClientId
+
+        if 'e' in msg and msg['e'] != 'ACCOUNT_UPDATE' and msg['e'] != 'TRADE_LITE' :
+            print(f"message type: {msg['e']}", datetime.datetime.today())
+            print(msg)
+            print('-------------------')
+
+            if 'e' in msg and msg['e'] == 'ORDER_TRADE_UPDATE':
+
+                if msg['o'].get('X') == 'FILLED':
+                    try:
+                        orderClientId = msg['o']['c']
+                        print('Исполнился ордер: ', orderClientId)
+                        print('Отменяем ордера:  ', datetime.datetime.today())
+                        print('-------------------')
+                        order = client.futures_cancel_all_open_orders(symbol=symbol)
+                    except BinanceAPIException as e:
+                        date = datetime.datetime.today()
+                        print('now ', date)
+                        print(e.status_code)
+                        print(e.message)
+                        print('-------------------')
+
+                elif msg['o'].get('X') == 'CANCELED':
+                    try:
+                        print('Отменился ордер: ', orderClientId)
+                        # date = datetime.datetime.today()
+                        print('now ', datetime.datetime.today())
+                        print('-------------------')
+                        lastPrice = orderClientId.replace("buy_", "")
+                        lastPrice = lastPrice.replace("sell_", "")
+                        lastPrice = float(lastPrice)
+                        zeroFiveProcent = lastPrice * gridPercent
+                        setGridOrders = {
+                            a: round(a * zeroFiveProcent + (lastPrice - zeroFiveProcent * quantityOrders / 2), lenDrobChast)
+                            for
+                            a in range(quantityOrders + 1)}
+
+                        priceBuyLimit = setGridOrders[int((len(setGridOrders) - 1) / 2) - 1]
+                        priceSellLimit = setGridOrders[int((len(setGridOrders) - 1) / 2) + 1]
+                        order = create_futures_buy_limit_order(symbol, sizeOrder,
+                                                                   priceBuyLimit,
+                                                                   'buy_' + str(priceBuyLimit))
+                        order = create_futures_sell_limit_order(symbol, sizeOrder,
+                                                                priceSellLimit,
+                                                                'sell_' + str(priceSellLimit))
+                    except BinanceAPIException as e:
+                        date = datetime.datetime.today()
+                        print('now ', date)
+                        print(e.status_code)
+                        print(e.message)
+                        print('-------------------')
+
+    # priceFilOdrer =
+
+    # Филл?   msg['o']['x'] == 'FILLED'
+    # Если да, то узнать цену msg['o']['p']
+    # Сравнить с центральной
+    # Выставить новый по центральной
+
+    client = Client(config.apiKey, config.secretKey
+                    # base_url="https://testnet.binance.vision"
+                    # proxies={ 'https': creds.proxy }
+                    )
+
+    # Подписываемся на WebSocket ордеров для отслеживания изменения (филл)
+    twm = ThreadedWebsocketManager(config.apiKey, config.secretKey)
+    # start is required to initialise its internal loop
+    twm.start()
+
+    date = datetime.datetime.today()
+    print('Start websocket now: ', date)
+
+    twm.start_futures_user_socket(callback=_user_data)
+    # twm.join()
+
+    df = DataFrame(
+        client.futures_recent_trades(**{'symbol': symbol, 'limit': 1})
+    )
+
+    while True:
+        if initProc:
+            initProc = False
+
+            lastPrice = float(df.iloc[0]['price'])
+            sizeOrder = int(sizeOrderUSD / float(df.iloc[0]['price']))
+
+            # Составить сетку ордеров
+            lenDrobChast = len(str(lastPrice).split(".")[1])
+            zeroFiveProcent = lastPrice * gridPercent
+            setGridOrders = {
+                a: round(a * zeroFiveProcent + (lastPrice - zeroFiveProcent * quantityOrders / 2), lenDrobChast) for a
+                in range(quantityOrders + 1)}
+
+            # Выставляем лимитные ордера на покупку
+            if 0 == 0:
+                lenSetGridOrders = len(setGridOrders)
+                for v in range(int((lenSetGridOrders - 1) / 2), 0, -1):
+                    order = create_futures_buy_limit_order(symbol, sizeOrder, setGridOrders[v - 1],
+                                                           'buy_' + str(setGridOrders[v - 1]))
+                    # print(setGridOrders[v-1])
+                    print(order)
+
+            # Выставляем лимитные ордера на продажу
+            if 0 == 0:
+                lenSetGridOrders = len(setGridOrders)
+                for v in range(int((lenSetGridOrders + 1) / 2), lenSetGridOrders):
+                    order = create_futures_sell_limit_order(symbol, sizeOrder, setGridOrders[v],
+                                                            'sell_' + str(setGridOrders[v]))
+                    print(order)
+        # else:
+
+        # print("* TRADE *", df.tail(10), sep="\n")
 
 
-while True:
+if __name__ == "__main__":
+    main()
+
+
+def mainkms():
     # Получить цену
-    trades = ccxtBinance.fetch_trades(symbol, limit=1)
-    lastPrice = trades[0]['price']
-    print(lastPrice)
+    #     x = {'symbol': symbol, 'limit': 1}
+    df = DataFrame(
+        client.futures_recent_trades(**{'symbol': symbol, 'limit': 1})
+    )
+    lastPrice = float(df.iloc[0]['price'])
+    sizeOrder = int(sizeOrderUSD / float(df.iloc[0]['price']))
+
     # Составить сетку ордеров
-    setOrders[11] = int(lastPrice)
     lenDrobChast = len(str(lastPrice).split(".")[1])
-    # zeroFiveProcent = round(lastPrice * 0.005, lenDrobChast)
-    zeroFiveProcent = lastPrice * 0.005
+    zeroFiveProcent = lastPrice * gridPercent
+    setGridOrders = {a: round(a * zeroFiveProcent + (lastPrice - zeroFiveProcent * quantityOrders / 2), lenDrobChast)
+                     for a in range(quantityOrders + 1)}
 
-    setOrders = {a: round(a * zeroFiveProcent + (lastPrice-zeroFiveProcent*10), lenDrobChast) for a in range(21)}
-    # setOrders01 = {a: a * zeroFiveProcent + (lastPrice - zeroFiveProcent * 10) for a in range(21)}
-    # try:
+    # sizeOrder = 280
+    # symbol = "BROCCOLIF3BUSDT"
+    # order = client.futures_create_order(
+    #     symbol=symbol,
+    #     side="BUY",
+    #     type="LIMIT",
+    #     quantity=sizeOrder,
+    #     # price=setGridOrders[v],
+    #     price=0.019500,
+    #     # positionSide="LONG",  # BOTH for One-way Mode ; LONG or SHORT for Hedge Mode
+    #     newClientOrderId=str('0.019'),
+    #     recvWindow=5000,
+    #     timeInForce="GTC",
+    #     timestamp=time.time_ns()
+    # )
+    # # print(DataFrame(order))
+    # print(order)
 
-    time.sleep(100)
+    # Выставляем лимитные ордера на покупку
+    if 0 != 0:
+        lenSetGridOrders = len(setGridOrders)
+        for v in range(int((lenSetGridOrders - 1) / 2), 0, -1):
+            order = client.futures_create_order(
+                symbol=symbol,
+                side="BUY",
+                type="LIMIT",
+                quantity=sizeOrder,
+                price=setGridOrders[v - 1],
+                # positionSide="SHORT",  # BOTH for One-way Mode ; LONG or SHORT for Hedge Mode
+                newClientOrderId=str(setGridOrders[v]),
+                recvWindow=5000,
+                timeInForce="GTC",
+                timestamp=time.time_ns()
+            )
+            # print(setGridOrders[v-1])
+            print(order)
 
+    print('--------------------')
+    # Выставляем лимитные ордера на продажу
+    if 0 != 0:
+        lenSetGridOrders = len(setGridOrders)
+        for v in range(int((lenSetGridOrders + 1) / 2), lenSetGridOrders):
+            order = client.futures_create_order(
+                symbol=symbol,
+                side="SELL",
+                type="LIMIT",
+                quantity=sizeOrder,
+                price=setGridOrders[v],
+                # positionSide="LONG",  # BOTH for One-way Mode ; LONG or SHORT for Hedge Mode
+                newClientOrderId=str(setGridOrders[v]),
+                recvWindow=5000,
+                timeInForce="GTC",
+                timestamp=time.time_ns()
+            )
 
+            # print(setGridOrders[v])
+            print(order)
 
-    trades = ccxtBinance.fetch_trades(symbol, limit=1)
-    print(trades)
+    # Подписываемся на WebSocket ордеров для отслеживания изменения (филл)
 
+    print("* TRADE *", df.tail(10), sep="\n")
 
+# mainKMS()
 
-
-logging.debug("closing ws connection")
-# webSocketClient.stop()
-
-
+# -----------------------------------------------------------------
+#     try:
+#
+#         # print(f"* exchange_info {symbol}* ", cl.exchange_info(symbol), sep="\n")
+#
+#         # print(f"* best_price {symbol}* ", cl.book_ticker(symbol=symbol), sep="\n")
+#         # buy_price = cl.book_ticker(symbol=symbol).get('askPrice')
+#         # sell_price = cl.book_ticker(symbol=symbol).get('bidPrice')
+#
+#
+#         print(cl.new_order(
+#             symbol=symbol,
+#             # newClientOrderId="sell_btcusdt_15",
+#             side='SELL',
+#             type='LIMIT',
+#             quantity=0.7,
+#
+#             price=30000,
+#             timeInForce='GTC'
+#         ))
+#
+#         print(f"* account assets * ", cl.account(), sep="\n")
+#
+#         # order_id = 8214572
+#         # print(f"* CANCEL order {order_id}", cl.cancel_order(symbol=symbol, orderId=order_id), sep="\n")
+#
+#         df = DataFrame(cl.get_orders(symbol, recvWindow=59000), columns=['price', 'origQty', 'executedQty', 'cummulativeQuoteQty', 'origQuoteOrderQty', 'status', 'type', 'side', 'orderId'])
+#         print("* ORDERS *", df.tail(10), sep="\n")
+#     except ClientError as e:
+#         print(e.error_code, e.error_message)
+#
+# --------------------------------------
+# client = UMFutures(key=config.apiKey, secret=config.secretKey)
+#
+# # link = 'wss://fstream.binance.com/ws/'+symbol.lower()+'@trade'
+#
+#
+# def message_handler(_, message):
+#     print(message)
+#
+#
+#
+# ccxtBinance = ccxt.binance({
+#             'apiKey': config.apiKey,
+#             'secret': config.secretKey,
+#             'enableRateLimit': True,
+#
+#             'options': {
+#                 'defaultType': 'future',
+#                 'urls': {
+#                     'api': 'fapiPublic',
+#                     'test': 'https://testnet.binancefuture.com',
+#                 },
+#             },
+#         })
+#
+#
+# while True:
+#     # Получить цену
+#     trades = ccxtBinance.fetch_trades(symbol, limit=1)
+#     lastPrice = trades[0]['price']
+#     print(lastPrice)
+#     # Составить сетку ордеров
+#     setOrders[11] = int(lastPrice)
+#     lenDrobChast = len(str(lastPrice).split(".")[1])
+#     # zeroFiveProcent = round(lastPrice * 0.005, lenDrobChast)
+#     zeroFiveProcent = lastPrice * 0.005
+#
+#     setOrders = {a: round(a * zeroFiveProcent + (lastPrice-zeroFiveProcent*10), lenDrobChast) for a in range(21)}
+#     # setOrders01 = {a: a * zeroFiveProcent + (lastPrice - zeroFiveProcent * 10) for a in range(21)}
+#     # try:
+#
+#     time.sleep(100)
+#
+#
+#
+#     trades = ccxtBinance.fetch_trades(symbol, limit=1)
+#     print(trades)
+#
+#
+#
+#
+# logging.debug("closing ws connection")
+# # webSocketClient.stop()
+#
+# -----------------------------------------------------------------
 
 
 # Проверять количество открытых ордеров
 # Если открытые уменьшились, то выставлять ближний в нужную сторону
-
 
 
 # webSocketClient = UMFuturesWebsocketClient(on_message=message_handler)
@@ -89,7 +394,6 @@ logging.debug("closing ws connection")
 # time.sleep(100)
 
 
-
 # Subscribe to a single symbol stream
 # print(webSocketClient.agg_trade(symbol="btcusdt")) #symbol.lower()))
 
@@ -98,14 +402,13 @@ logging.debug("closing ws connection")
 # print("closing ws connection")
 # webSocketClient.stop()
 
-while True:
-    print('.')
+# while True:
+#     print('.')
 
 
 # print(client.exchange_info())
 # print(client.balance())
 # print(client.new_order(symbol=symbol, side='BUY', type='LIMIT', price=0.04, quantity=1000, timeInForce="GTC"))
-
 
 
 #
